b2fff0ef5d4093fd3a7d34b3e3c0bbf7970d8bde
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:26:38 +0530

    Add files via upload

---
+++ Degree Celcius to Degree Fahrenheit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter Temperature in Degree Celcius :"))
+print("TheTemperature in Degree Fahrenheit is :",((a*9)/5)+32)
+
+
+# In[ ]:
+
+
+
+
