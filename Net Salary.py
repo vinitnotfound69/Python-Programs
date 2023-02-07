617e00a8c402f8b968bb91666743416c4b21e90f
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-08 00:11:40 +0530

    Add files via upload

---
+++ Net Salary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=float(input("Enter the Gross salary :"))
+print("The allowance is 10% of Gross salary is :",(a/10))
+print("The Deduction is 3% of Gross salary is :",(a*3/100))
+b=a+(a/10)
+c=a-(a*3/100)
+print("The Net salary is :",a+b-c)
+
+
+# In[ ]:
+
+
+
+
---
+++ Net sales with Discount.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter the Gross sales :"))
+print("The Discount is 10% of Gross sales are :",(a/10))
+b=(a/10)
+print("The Net sales are :",a-b)
+
+
+# In[ ]:
+
+
+
+
