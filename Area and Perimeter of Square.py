0db81a58ac6224ea2f3875d2b50e0e5c9cb6b1ea
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:53:54 +0530

    Add files via upload

---
+++ Area and Perimeter of Square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=float(input("Enter the length value :"))
+print("The Perimeter of Square is :",a*4)
+print("The Area of Square is :",a*a)
+
+
+# In[ ]:
+
+
+
+
