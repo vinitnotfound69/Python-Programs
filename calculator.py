4b99654b7e3823ce797d6929482046b6906092d2
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 22:47:13 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("The Addition of given two numbers is :",a+b)
+print("The Substration of given two numbers is :",a-b)
+print("The Multiplication of given two numbers is :",a*b)
+print("The Division of given two numbers is :",a/b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
