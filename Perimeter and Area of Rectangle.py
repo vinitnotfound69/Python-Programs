a55264b45dd69820402e84281a489adb63bef2e3
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:56:18 +0530

    Add files via upload

---
+++ Perimeter and Area of Rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter the length value :"))
+b=float(input("Enter the breadth value :"))
+print("The Perimeter of Rectangle is :",a+a+b+b)
+print("The Area of Rectangle is :",a*b)
+
+
+# In[ ]:
+
+
+
+
