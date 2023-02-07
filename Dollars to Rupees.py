2d4311d648b2fc91835885a9571b7e66fd3f648e
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:03:58 +0530

    Add files via upload

---
+++ Dollars to Rupees.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter money in Dollars:"))
+print("The money value in rupees where one Dollar equals 48 Rupees is :",a*48)
+
+
+# In[ ]:
+
+
+
+
