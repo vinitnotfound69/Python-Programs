c6fb36fdb344517884c4872b49d79cf343df2087
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:14:50 +0530

    Add files via upload

---
+++ Bytes to Kilobytes, Megabytes, Gigabytes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=float(input("Enter Memory in Bytes:"))
+print("The Memory in Kilobytes is :",a/1000)
+print("The Memory in Kilobytes is :",a/1000000)
+print("The Memory in Kilobytes is :",a/1000000000)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
