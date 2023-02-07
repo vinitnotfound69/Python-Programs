5dedd81220bf0a8cb0640db22e2b8cf19b6faabd
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:33:44 +0530

    Add files via upload

---
+++ Simple Interest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter the Principle value :"))
+b=float(input("Enter the Rate of Interest:"))
+c=float(input("Enter the Time in Years :"))
+print("The Simple Interest is gibe by :",a*b*c/100)
+
+
+# In[ ]:
+
+
+
+
