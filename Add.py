cc741d5f075f3c280f0333d02f3eb2a9281e5081
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 22:32:47 +0530

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("The addition of given two numbers is :",a+b)
+
+
+# In[ ]:
+
+
+
+
