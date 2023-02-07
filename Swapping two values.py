3ca5821322890c0c4dc37f8493f4faae27e5579b
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-08 00:18:41 +0530

    Add files via upload

---
+++ Swapping two values.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("Enter a value to be swaped :"))
+b=int(input("Enter an other value to be swaped :"))
+c=a
+a=b
+b=c
+print("The value of 'a' and 'b' after swapping are respectively :",a,b)
+
+
+# In[ ]:
+
+
+
+
