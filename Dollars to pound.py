24089a3a872599dea05a453a982899a0096d1942
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 23:07:39 +0530

    Add files via upload

---
+++ Dollars to pound.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=float(input("Enter money in Dollars:"))
+print("The money value in Pounds where one Dollar equals 48 Rupees and one Pound equals 70 Rupees is :",(a*48)/70)
+
+
+# In[ ]:
+
+
+
+
