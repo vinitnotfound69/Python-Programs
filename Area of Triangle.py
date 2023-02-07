bd6b15380db58567f7deac2749b0382c64d973e7
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-08 00:01:07 +0530

    Add files via upload

---
+++ Area of Triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter the Height :"))
+b=float(input("Enter the Width :"))
+print("The Area of Triangle is :",a*b/2)
+
+
+# In[ ]:
+
+
+
+
