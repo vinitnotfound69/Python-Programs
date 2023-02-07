2f466ebc63f9930fcd263005b73941910f4aa590
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-08 00:15:00 +0530

    Add files via upload

---
+++ Total and Average of Marks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=float(input("Enter the Marks of first subject :"))
+b=float(input("Enter the Marks of second subject :"))
+c=float(input("Enter the Marks of third subject :"))
+print("The Total of marks is :",a+b+c)
+print("The Average of marks is :",(a+b+c)/3)
+
+
+# In[ ]:
+
+
+
+
