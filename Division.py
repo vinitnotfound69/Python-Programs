590cb1efd0d13f8b4708070942e9b4fe81817662
vinitnotfound69 <124489011+vinitnotfound69@users.noreply.github.com>
2023-02-07 22:36:56 +0530

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("The Multiplication of given two numbers is :",a/b)
+
+
+# In[ ]:
+
+
+
+
